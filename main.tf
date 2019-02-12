9a2d9ed1f75d7322e31567693243698397adf822
Robin Diddams <robindiddams@gmail.com>
2019-02-12 08:51:54 -0500

    added dynamodb table🍽

--- main.tf
+++ main.tf
@@ -13,20 +13,21 @@ resource "aws_s3_bucket" "post-bucket" {
   acl    = "private"
 }
 
-# resource "aws_dynamodb_table" "lyrebird-events" {
-#   name = "events"
-#   read_capacity = 5
-#   write_capacity = 5
-#   hash_key = "id"
-#   range_key = "timestamp"
+resource "aws_dynamodb_table" "lyrebird-tasks" {
+  name = "lyrebird-tasks"
+  hash_key = "id"
 
-#   attribute = [
-#     {
-#       name = "id"
-#       type = "S"
-#     },
-#     {
-#       name = "timestamp"
-#       type = "N"
-#     }]
-# }
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  global_secondary_index = []
+  read_capacity = 5
+  write_capacity = 5
+  stream_enabled = false
+
+  lifecycle = {
+    ignore_changes = ["read_capacity", "write_capacity"]
+  }
+}
